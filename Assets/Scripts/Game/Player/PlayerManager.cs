ad5497c88a7c5d99abefe9f664d14598f3d38377
Ben Schneider <benscr.email@gmail.com>
2026-02-21 16:26:34 +0100

    Update

--- Assets/Scripts/Game/Player/PlayerManager.cs
+++ Assets/Scripts/Game/Player/PlayerManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,17 +15,31 @@ namespace BenScr.MinecraftClone
         [SerializeField] private float breakBlockCooldown = 0.1f;
         [SerializeField] private float placeBlockCooldown = 0.1f;
         [SerializeField] private Block selectedBlock;
+
+        [SerializeField] private Image prevSelectedBlockPreview;
         [SerializeField] private Image selectedBlockPreview;
+        [SerializeField] private Image nextSelectedBlockPreview;
+        [SerializeField] private Image nextSelectedBlockPreviewAnim;
+        [SerializeField] private Animator animator;
 
         private Vector3 highlightPosition;
         private Vector3 placeBlockPosition;
         private bool isHighlightBlockVisible = false;
+        public float dur = 15;
+        private bool isAnimating = false;
 
         private float breakBlockTimer = 0f;
         private float placeBlockTimer = 0f;
 
         private void Awake()
         {
+            UpdateBlocksUI();
+        }
+
+        private void UpdateBlocksUI()
+        {
+            prevSelectedBlockPreview.sprite = GetNextBlock().preview;
+            nextSelectedBlockPreview.sprite = GetPrevBlock().preview;
             selectedBlockPreview.sprite = selectedBlock.preview;
         }
 
@@ -41,17 +57,33 @@ namespace BenScr.MinecraftClone
             breakBlockTimer += Time.deltaTime;
             placeBlockTimer += Time.deltaTime;
 
-            if (Input.mouseScrollDelta.y >= 1)
+            if (Input.mouseScrollDelta.y >= 1 && !isAnimating)
             {
-                int blocksCount = AssetsContainer.instance.blocks.Length;
-                selectedBlock = AssetsContainer.GetBlock(math.clamp((selectedBlock.id + 1) % blocksCount, 1, blocksCount - 1));
-                selectedBlockPreview.sprite = selectedBlock.preview;
+                animator.SetTrigger("Next");
+                isAnimating = true;
+
+                nextSelectedBlockPreviewAnim.sprite = GetNextBlock(2).preview;
+
+                StartCoroutine(DoAfterDelay(dur, () =>
+                {
+                    selectedBlock = GetNextBlock();
+                    UpdateBlocksUI();
+                    isAnimating = false;
+                }));
             }
-            else if (Input.mouseScrollDelta.y == -1)
+            else if (Input.mouseScrollDelta.y == -1 && !isAnimating)
             {
-                int blocksCount = AssetsContainer.instance.blocks.Length;
-                selectedBlock = AssetsContainer.GetBlock(math.clamp((selectedBlock.id - 1 + (blocksCount - 1)) % blocksCount, 1, blocksCount - 1));
-                selectedBlockPreview.sprite = selectedBlock.preview;
+                animator.SetTrigger("Previous");
+                isAnimating = true;
+
+                nextSelectedBlockPreviewAnim.sprite = GetPrevBlock(2).preview;
+
+                StartCoroutine(DoAfterDelay(dur, () =>
+                {
+                    selectedBlock = GetPrevBlock();
+                    UpdateBlocksUI();
+                    isAnimating = false;
+                }));
             }
 
             if (isHighlightBlockVisible)
@@ -76,6 +108,30 @@ namespace BenScr.MinecraftClone
             }
 
             UpdateHighlightBlock();
+        }
+
+        private IEnumerator DoAfterDelay(float delay, Action action)
+        {
+            yield return new WaitForSeconds(delay);
+            action();
+        }
+
+        private Block GetPrevBlock(int backward = 1)
+        {
+            int blocksCount = AssetsContainer.instance.blocks.Length;
+
+            int n = blocksCount - 1;
+            int cur = selectedBlock.id - 1;
+
+            int prev = (cur - backward % n + n) % n;
+            int id = prev + 1;
+
+            return AssetsContainer.GetBlock(id);
+        }
+        private Block GetNextBlock(int forward = 1)
+        {
+            int blocksCount = AssetsContainer.instance.blocks.Length;
+            return AssetsContainer.GetBlock(math.clamp((selectedBlock.id + forward) % blocksCount, 1, blocksCount - 1));
         }
 
         private void UpdateHighlightBlock()
